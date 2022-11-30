a08b3e60d8baae429c29a81c77b705249b6994c5
Maciej Niszczota <macni.macni@gmail.com>
2022-11-30 23:25:15 +0100

    Initial project structure

---
+++ src/main/java/com/example/notatex/NotatexApplication.java
@@ -0,0 +1,13 @@
+package com.example.notatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotatexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotatexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notatex/NotatexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notatex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotatexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
