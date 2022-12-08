70b979320241d55b1aa3706c6210e105143e1619
Patryk Ferenc <patrykferenc@protonmail.com>
2022-12-08 14:22:18 +0100

    Adjust directory structure.

---
+++ notatex-app/src/test/java/com/example/notatex/e2e/CucumberAcceptanceRunnerTest.java
@@ -0,0 +1,18 @@
+package com.example.notatex.e2e;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/scenarios/acceptance",
+        plugin = {"pretty", "html:target/cucumber-reports/report.html", "json:target/cucumber-reports/cucumber-json-report.json"},
+        glue = {"com.example.notatex.e2e.stepDefinition"}
+)
+
+public class CucumberAcceptanceRunnerTest {
+
+}
