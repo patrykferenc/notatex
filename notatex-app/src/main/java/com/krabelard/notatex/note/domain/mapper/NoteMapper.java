2c4bf43b2aa4ca20859df724539e876742d17c85
Jakub Łaba <jlaba@griddynamics.com>
2023-01-11 17:11:53 +0100

    Fix NoteMapper

--- notatex-app/src/main/java/com/krabelard/notatex/note/domain/mapper/NoteMapper.java
+++ notatex-app/src/main/java/com/krabelard/notatex/note/domain/mapper/NoteMapper.java
@@ -3,6 +3,7 @@ package com.krabelard.notatex.note.domain.mapper;
 import com.krabelard.notatex.note.domain.dto.NoteDTO;
 import com.krabelard.notatex.note.domain.model.Note;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,7 @@ public interface NoteMapper {
 
     NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);
 
+    @Mapping(target = "contents", ignore = true)
     Note dtoToEntity(NoteDTO dto);
 
     NoteDTO entityToDto(Note entity);
