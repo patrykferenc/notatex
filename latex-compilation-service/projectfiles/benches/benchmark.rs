26a08b07960246140f232457077789c41f7f6c3a
michalskim <72550341+legeof008@users.noreply.github.com>
2023-01-13 14:47:41 +0100

    fix: delete redundant dockerfile option (#32)

--- latex-compilation-service/projectfiles/benches/benchmark.rs
+++ latex-compilation-service/projectfiles/benches/benchmark.rs
@@ -15,21 +15,21 @@ fn run_cleanup_routine(c: &mut Criterion){
 fn run_external_executable(c: &mut Criterion){
     let argument = black_box("hello");
     c.bench_function(
-        "run_cleanup_routine",
+        "run_external_executable",
         |b| b.iter(||latex_utilization_bench::run_external_executable(&String::from("echo"),argument))
     );
 
 }
 fn run_create_expected_output_path(c: &mut Criterion){
     c.bench_function(
-        "run_cleanup_routine",
+        "run_create_tex_output",
         |b| b.iter(||latex_utilization_bench::create_expected_output_path(String::from("/tmp/file")))
     );
 
 }
 fn run_was_compilation_succesfull(c: &mut Criterion){
     c.bench_function(
-        "run_cleanup_routine",
+        "run_compilation_check",
         |b| b.iter(||latex_utilization_bench::was_compilation_succesfull(&String::from("/tmp/test.db")))
     );
 
